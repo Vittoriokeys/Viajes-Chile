e7002b6f7b804d259acd49243437d7571e567825
Víctor Subiabre De Girolamo del Mauro <victor.subiabre.d@gmail.com>
2024-03-28 10:51:41 -0300

    Se agrega sección contacto

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,21 @@ $(function () {
         }
     });
 });
+
+// Función para el smooth scroll ajustado a 200 ms
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 200);
+        }
+    });
+});
+
+//Función para el tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
